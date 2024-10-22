2d1ced5a712fc969958d96f62dd6670ebea7f4a2
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-10-22 15:06:55 +0530

    create server

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use((req,res)=>{
+    res.send("hello from the server");
+})
+app.listen(3000,()=>{
+    console.log("server is listening on port no 3000")
+});
